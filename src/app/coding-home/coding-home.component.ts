b686666a31af5b63280012c0fa900efee94fec72
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-02-27 16:10:43 -0500

    add coding-home folder. Everything is working.

---
+++ src/app/coding-home/coding-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coding-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './coding-home.component.html',
+  styleUrl: './coding-home.component.scss'
+})
+export class CodingHomeComponent {
+
+}
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./../header/header.component";
+import { CodingHomeComponent } from "./../coding-home/coding-home.component";
+import { FooterComponent } from "./../footer/footer.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     HeaderComponent,
-    
+    CodingHomeComponent,
+    FooterComponent,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
