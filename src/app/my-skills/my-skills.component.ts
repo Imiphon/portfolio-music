eef525779ffec0d49aa30b24d1d0e5e1655f5428
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-03-09 15:45:28 -0500

    add bubbles for skill-description

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
 
+  showPopup = false;
+  popupContent = '';
+
+  popupPositionX:string = '0px';
+  popupPositionY:string = '0px';
+
+  openPopup(name: string, event:MouseEvent): void {
+    this.popupContent = name;
+    this.showPopup = true;
+    // Ermittle die Position des angeklickten Elements
+    const clickX = event.clientX;
+    const clickY = event.clientY;
+
+    console.log(clickX, clickY);
+    
+    // Setze die Popup-Position
+    this.popupPositionX = clickX + 'px';
+    this.popupPositionY = clickY + 'px';
+    setTimeout(() => {
+      this.showPopup = false;
+    }, 3000);
+  }
 }
