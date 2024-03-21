2fe0621c412cce01875be07e3931daef3493de68
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-03-21 12:41:42 -0400

    add zoom for about-me-text

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LanguageService } from "./../language.service";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [
-    //HeaderComponent,
+    CommonModule
   ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent implements OnInit {
   aboutMeText: string = '';
+  showTextOverlay: boolean = false;
   constructor(private languageService: LanguageService, private http: HttpClient ) {}
 
   ngOnInit(): void {
@@ -20,7 +23,7 @@ export class AboutMeComponent implements OnInit {
       this.onLanguageChange(lang);
     })
   }
-
+  //maby necassary if it used again in btns directly in this component.
   onLanguageChange(lang: string): void {
     this.loadText(lang); 
   }
@@ -29,25 +32,10 @@ export class AboutMeComponent implements OnInit {
     this.http.get<any>('assets/text-data.json').subscribe(data => {
       this.aboutMeText = data[lang]['aboutMe'];
     });
+  }
+
+  toggleTextOverlay() {
+    // Diese Methode schaltet den Zustand des Overlays um (anzeigen/verstecken)
+    this.showTextOverlay = !this.showTextOverlay;
   }
 }
-
-/**
- *  
-  ngOnInit(): void {  
-    //standard language     
-    this.loadText('en'); 
-  }
-
-  loadText(lang: string): void {
-    this.http.get<any>('assets/text-data.json').subscribe(data => {
-      this.aboutMeText = data[lang]['aboutMe'];
-    });
-  }
-
-  onLanguageChange(lang: string): void {
-    debugger
-    console.log('onLanguageChanged in about-me.comp runs');
-    this.loadText(lang); 
-  }
- */
